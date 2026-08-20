742ca99d5a809e205524cc4a863393dfb2151dcf
Eldest666 <besana0203@gmail.com>
2026-08-20 14:44:51 +0200

    Add files via upload

---
+++ Mappa valori percentuali/layers/layers.js
@@ -0,0 +1,41 @@
+var wms_layers = [];
+
+
+        var lyr_OSMStandard_0 = new ol.layer.Tile({
+            'title': 'OSM Standard',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: '<a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var format_LocalittedeschedellaDobrugia1930_1 = new ol.format.GeoJSON();
+var features_LocalittedeschedellaDobrugia1930_1 = format_LocalittedeschedellaDobrugia1930_1.readFeatures(json_LocalittedeschedellaDobrugia1930_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_LocalittedeschedellaDobrugia1930_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_LocalittedeschedellaDobrugia1930_1.addFeatures(features_LocalittedeschedellaDobrugia1930_1);
+var lyr_LocalittedeschedellaDobrugia1930_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_LocalittedeschedellaDobrugia1930_1, 
+                style: style_LocalittedeschedellaDobrugia1930_1,
+                popuplayertitle: 'Località tedesche della Dobrugia (1930)',
+                interactive: true,
+    title: 'Località tedesche della Dobrugia (1930)<br />\
+    <img src="styles/legend/LocalittedeschedellaDobrugia1930_1_0.png" /> 0 - 20<br />\
+    <img src="styles/legend/LocalittedeschedellaDobrugia1930_1_1.png" /> 20 - 50<br />\
+    <img src="styles/legend/LocalittedeschedellaDobrugia1930_1_2.png" /> 50 - 70<br />\
+    <img src="styles/legend/LocalittedeschedellaDobrugia1930_1_3.png" /> 70 - 90<br />\
+    <img src="styles/legend/LocalittedeschedellaDobrugia1930_1_4.png" /> 90 - 100<br />' });
+
+lyr_OSMStandard_0.setVisible(true);lyr_LocalittedeschedellaDobrugia1930_1.setVisible(true);
+var layersList = [lyr_OSMStandard_0,lyr_LocalittedeschedellaDobrugia1930_1];
+lyr_LocalittedeschedellaDobrugia1930_1.set('fieldAliases', {'Località': 'Località', 'Toponimo tedesco': 'Toponimo tedesco', 'Plasă': 'Plasă', 'Județ': 'Județ', 'Regione storica': 'Regione storica', 'Popolazione complessiva': 'Popolazione complessiva', 'Numero abitanti romeni': 'Numero abitanti romeni', 'Percentuale abitanti romeni': 'Percentuale abitanti romeni', 'Numero abitanti tedeschi': 'Numero abitanti tedeschi', 'Percentuale abitanti tedeschi': 'Percentuale abitanti tedeschi', 'Numero abitanti di altra etnia': 'Numero abitanti di altra etnia', 'Percentuale abitanti di altra etnia': 'Percentuale abitanti di altra etnia', 'Latitudine N': 'Latitudine N', 'Longitudine E': 'Longitudine E', });
+lyr_LocalittedeschedellaDobrugia1930_1.set('fieldImages', {'Località': 'TextEdit', 'Toponimo tedesco': 'TextEdit', 'Plasă': 'TextEdit', 'Județ': 'TextEdit', 'Regione storica': 'TextEdit', 'Popolazione complessiva': 'Range', 'Numero abitanti romeni': 'Range', 'Percentuale abitanti romeni': 'TextEdit', 'Numero abitanti tedeschi': 'Range', 'Percentuale abitanti tedeschi': 'TextEdit', 'Numero abitanti di altra etnia': 'Range', 'Percentuale abitanti di altra etnia': 'TextEdit', 'Latitudine N': 'TextEdit', 'Longitudine E': 'TextEdit', });
+lyr_LocalittedeschedellaDobrugia1930_1.set('fieldLabels', {'Località': 'inline label - always visible', 'Toponimo tedesco': 'inline label - always visible', 'Plasă': 'inline label - always visible', 'Județ': 'inline label - always visible', 'Regione storica': 'inline label - always visible', 'Popolazione complessiva': 'inline label - always visible', 'Numero abitanti romeni': 'inline label - always visible', 'Percentuale abitanti romeni': 'inline label - always visible', 'Numero abitanti tedeschi': 'inline label - always visible', 'Percentuale abitanti tedeschi': 'inline label - always visible', 'Numero abitanti di altra etnia': 'inline label - always visible', 'Percentuale abitanti di altra etnia': 'inline label - always visible', 'Latitudine N': 'inline label - always visible', 'Longitudine E': 'inline label - always visible', });
+lyr_LocalittedeschedellaDobrugia1930_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ Mappa valori percentuali/styles/LocalittedeschedellaDobrugia1930_1_style.js
@@ -0,0 +1,68 @@
+var size = 0;
+var placement = 'point';
+
+var style_LocalittedeschedellaDobrugia1930_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("Percentuale abitanti tedeschi");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = 'left';
+    var offsetX = 8;
+    var offsetY = 3;
+    var overflow = false;
+    var repeat = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= 0.000000 && value <= 20.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 8.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(255,255,255,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(4,252,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })]
+                    } else if (value >= 20.000000 && value <= 50.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 8.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(255,255,255,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(23,255,11,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })]
+                    } else if (value >= 50.000000 && value <= 70.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 8.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(255,255,255,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(242,222,27,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })]
+                    } else if (value >= 70.000000 && value <= 90.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 8.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(255,255,255,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(255,183,64,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })]
+                    } else if (value >= 90.000000 && value <= 100.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 8.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(50,87,128,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(182,9,23,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })]
+                    };
+
+    return style;
+};
